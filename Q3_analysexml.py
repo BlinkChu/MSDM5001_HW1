b9eb0610efd899cffaac0687d092956751975bb1
DengKaifang <41252802+BlinkChu@users.noreply.github.com>
2020-10-19 16:57:35 +0800

    Update Q3_analysexml.py

--- Q3_analysexml.py
+++ Q3_analysexml.py
@@ -12,12 +12,11 @@ blockid_lst = re.findall(block_pattern,xml_content)
 ID_pattern = '(?P<blockid><emItem blockID=".+" id=".+@.+">)'
 emails_lst = re.findall(ID_pattern,xml_content)
 
-# print out the
-print('Now print out the text lines with the “blockID” values that start with the letter “i” or “g”, and end with digits')
+print('Now print out the text lines where the “ID” values are email addresses')
 for i in emails_lst:
     if not (('/' in i) or ('/' in i) or ('^' in i)):
         print(i)
 
-print('Now print out the text lines where the “ID” values are email addresses')
+print('Now print out the text lines with the “blockID” values that start with the letter “i” or “g”, and end with digits')
 for i in blockid_lst:
     print(i)
